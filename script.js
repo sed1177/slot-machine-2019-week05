b33ff8526aee58e5acac48535cde78a0dd3d9a57
sed1177 <shawnholmeswork@gmail.com>
2025-09-23 23:18:53 -0400

    added an extra slot

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ const resetBtn = document.getElementById('resetBtn')
 const slot1 = document.getElementById('slot1')
 const slot2 = document.getElementById('slot2')
 const slot3 = document.getElementById('slot3')
+const slot4 = document.getElementById('slot4')
 const result = document.getElementById('result')
 const balanceDisplay = document.getElementById('balance')
 
 // storing shows in an array
-const shows = ["Money Heist", "Love Island", "Prison Break", "Breaking Bad", "Stranger Things"]
+const shows = ["Money Heist", "Love Island", "Prison Break", "Breaking Bad", "Stranger Things", "Naruto", "Monsters Inc"]
 
 // users balance
 let balance = 0
 
 // getting a random show from the array!
 function randomShow() {
+  // returns the array with the index as a random show from the array 
   return shows[Math.floor(Math.random() * shows.length)]
 }
 
@@ -25,19 +27,20 @@ spinBtn.addEventListener('click', () => {
   const show1 = randomShow()
   const show2 = randomShow()
   const show3 = randomShow()
+  const show4 = randomShow()
 
 //  put the array value in the UI
   slot1.textContent = show1
   slot2.textContent = show2
   slot3.textContent = show3
+  slot4.textContent = show4
 
 //   core logic
-
-  if (show1 === show2 && show2 === show3) {
+  if (show1 === show2 && show2 === show3 && show3 === show4) {
     result.textContent = "🎉 Jackpot! All three match! You won $500!"
     result.style.color = "green"
     balance += 500
-  } else if (show1 === show2 || show2 === show3 || show1 === show3) {
+  } else if (show1 === show2 || show2 === show3 || show1 === show3 || show1 === show4 || show2 === show4) {
     result.textContent = "👏 Nice! Two shows match! You won $250!"
     result.style.color = "orange"
     balance += 250
@@ -46,11 +49,11 @@ spinBtn.addEventListener('click', () => {
     result.style.color = "red"
   }
 
-//   users balance
+//  users balance
   balanceDisplay.textContent = `💵 Balance: $${balance}`
 })
 
-// reset balace to ZERO and put ? in each box 
+// reset balace to ZERO and put ? in each show box 
 resetBtn.addEventListener('click', () => {
   balance = 0
   balanceDisplay.textContent = "💵 Balance: $0"
@@ -58,6 +61,7 @@ resetBtn.addEventListener('click', () => {
   slot1.textContent = "?"
   slot2.textContent = "?"
   slot3.textContent = "?"
+  slot4.textContent = "?"
 })
 
 
